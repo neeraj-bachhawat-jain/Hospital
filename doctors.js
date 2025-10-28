291875ef2c1ff29b6ee35f5cf300c195e54d1ccf
Neeraj Jain <neeraj2003.jain@gmail.com>
2025-10-28 12:45:16 +0530

    routes

---
+++ doctors.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Doctor = require('../Model/doctor');
+
+router.post('/signup', async (req, res)=>{
+  
+}) 
